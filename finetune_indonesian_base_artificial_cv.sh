58b9586b006344beef65e6505710725766e62026
Cahya Wirawan <cahya.wirawan@gmail.com>
2021-04-28 02:07:35 +0200

    added artificial fine tuning

--- finetune_indonesian_base_artificial_cv.sh
+++ finetune_indonesian_base_artificial_cv.sh
@@ -4,4 +4,4 @@ export WANDB_ENTITY="cahya"
 export WANDB_PROJECT="wav2vec2-base-indonesian"
 export WANDB_LOG_MODEL="true"
 
-python run_common_voice_indonesian_artificial.py indonesian_base_artificial.json
+python run_common_voice_indonesian.py indonesian_base_artificial_cv.json
