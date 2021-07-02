1d7272f26cd3301df0f34ea513665ae783e4de5a
Cahya Wirawan <Cahya.Wirawan@gmail.com>
2021-07-02 11:49:16 +0200

    fixed the test script

--- finetune_indonesian_base.sh
+++ finetune_indonesian_base.sh
@@ -4,8 +4,6 @@ export WANDB_ENTITY="cahya"
 export WANDB_PROJECT="wav2vec2-base-indonesian"
 export WANDB_LOG_MODEL="true"
 
-python run_common_voice_indonesian.py indonesian_base.json \
-  --num_train_epochs="2" \
-  --output_dir="/workspace/output_models/wav2vec2-base-cv"
+python run_common_voice_indonesian.py indonesian_base.json
 
 
